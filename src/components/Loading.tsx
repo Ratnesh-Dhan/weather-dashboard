68e96233c31cfb133c46d30bba98ac34dc2746c9
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-05-28 17:46:15 +0530

    added clicking sound on every second on the clock & UI improvements

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Loading = () => {
-  const [load, setLoad] = useState(0);
-  useEffect(()=> {
-    
-  },[load]);
+  const [load] = useState(0);
+  useEffect(() => {}, [load]);
 
   return (
-    <div className='w-100vw h-screen bg-blue-300 relative'>
-        <div className="absolute bottom-0 bg-blue-600 w-[100vw]" style={{height:  `${load}%`}}>
-            Loading
-        </div>
+    <div className="w-100vw h-screen bg-blue-300 relative">
+      <div
+        className="absolute bottom-0 bg-blue-600 w-[100vw]"
+        style={{ height: `${load}%` }}
+      >
+        Loading
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
